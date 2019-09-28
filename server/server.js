036b19f256aac56569999a37933e14d0f76901f6
elise <elise.marie.lau@gmail.com>
2019-09-28 12:16:41 +0200

    rm unused router

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@
 const express = require("express"); 
 const mongoose = require("mongoose"); 
 const bodyParser = require('body-parser');
-const router = express.Router();
 
 //Import routes
 const user = require('./routes/user');
